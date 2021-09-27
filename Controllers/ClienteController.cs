0274ff7a5df02fe0c6cb956f87954ff02d90c18a
joaodemutti <joaofacindemutti@gmail.com>
2021-09-26 21:09:13 -0300

    cliente registro em cache

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -13,18 +13,42 @@ namespace Bicycles.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         public const string SessionKeyLogin = "_login";
-
+        public static List<ClienteModel> clientes = new List<ClienteModel>{};
+         public static ClienteModel ClienteRegistro;
 
         public ClienteController(ILogger<HomeController> logger)
         {
-            
-            _logger = logger;
+           _logger = logger;
         }
         
         public IActionResult Index()
         {
             if(HttpContext.Session.GetString(SessionKeyLogin)==null)return RedirectToAction("Index","Home",new {area=""});
             return View();
+        }
+
+        public IActionResult Registrar()
+        {
+            if(HttpContext.Session.GetString(SessionKeyLogin)==null)return RedirectToAction("Index","Home",new {area=""});
+            return View();
+        }
+
+        public IActionResult Consulta(int cpf)
+        {
+            ClienteRegistro=new ClienteModel();
+            ClienteRegistro.cpf_cli = cpf;
+            if(HttpContext.Session.GetString(SessionKeyLogin)==null)return RedirectToAction("Index","Home",new {area=""});
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registrar(ClienteModel cliente)
+        {
+            if(cliente.cpf_cli==null||cliente.nome_cli==null||cliente.telefone_cli==null||cliente.email_cli==null||cliente.cep_end==null)
+            return View(cliente);
+            
+            clientes.Add(cliente);
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Models/ClienteModel.cs
+++ Models/ClienteModel.cs
@@ -4,11 +4,11 @@ namespace Bicycles.Models
 {
     public class ClienteModel
     {
-        public int cpf_cli {get;set;}
+        public Nullable<int> cpf_cli {get;set;}
         public string nome_cli {get;set;}
         public string telefone_cli {get;set;}
         public string email_cli {get;set;}
-        public int cep_end {get;set;}
+        public Nullable<int> cep_end {get;set;}
 
         public ClienteModel(int cpf_cli,string nome_cli,string telefone_cli,string email_cli,int cep_end)
         {
@@ -17,6 +17,15 @@ namespace Bicycles.Models
             this.telefone_cli = telefone_cli;
             this.email_cli = email_cli;
             this.cep_end = cep_end;            
+        }
+
+        public ClienteModel()
+        {
+            this.cpf_cli = null;
+            this.nome_cli = null;
+            this.telefone_cli = null;
+            this.email_cli = null;
+            this.cep_end = null;            
         }
         
     }
